d3f5edb63b2650e5a3c9b290eaf57db3110382d9
Lorenzo <lorenzopelaez1977@gmail.com>
2021-12-06 21:25:14 +0100

    Ejercicio Mayoral

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function burguer() {
+    var x = document.getElementById("navegacion");
+
+    if (x.className === "barra") {
+        x.className += " desplegable";
+    }else {
+        x.className = "barra";
+    }
+}
+
+function pill() {
+    var x =  document.getElementById('navegacion').getElementsByClassName('pills'); 
+    for(i = 0; i < x.length; i++) { 
+        if(document.location.href.indexOf(x[i].href) >= 0) {
+            //El método indexOf () devuelve la posición de la primera aparición de un valor especificado en una cadena.
+            x[i].className =' hover';   
+        }
+    }
+}
+window.onload = pill;
+
+const btns = document.querySelectorAll(".filter");
+const storeProducts = document.querySelectorAll(".col");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function (e) {
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+
+    // Switch Tab content
+    const filter = e.target.dataset.filter;
+    // console.log(filter);
+    storeProducts.forEach((product) => {
+      if (filter === "all") {
+        product.style.display = "block";
+      } else if (product.classList.contains(filter)) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  });
+}
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".descripcion h5");
+const noResult = document.querySelector(".no-result");
+
+search.addEventListener("keyup", filterProducts);
+
+function filterProducts(e) {
+  const text = e.target.value.toLowerCase();
+
+  productName.forEach((product) => {
+    const item = product.textContent;
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      product.parentElement.parentElement.style.display = "block";
+      noResult.style.display = "none";
+    } else {
+      product.parentElement.parentElement.style.display = "none";
+      noResult.style.display = "block";
+    }
+  });
+}
